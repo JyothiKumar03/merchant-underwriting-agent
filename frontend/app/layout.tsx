9ff6fd909f52d53685a4b07fe606ff8ec3872311
JyothiKumar03 <djdummala@gmail.com>
2026-04-04 23:19:00 +0530

    fix: move of global.css

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Figtree } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 import { cn } from "@/lib/utils";
 
 const figtree = Figtree({subsets:['latin'],variable:'--font-sans'});
