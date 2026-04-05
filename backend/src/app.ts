0cf4ab5192f6f9b6d1337c967f780db147d492c2
JyothiKumar03 <djdummala@gmail.com>
2026-04-05 18:04:18 +0530

    feat: layout + config setup

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import router from "./routes/index.js";
+import cors from "cors";
+
 
 const app = express();
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
+app.use(cors());
+
 
 app.use("/api/v1", router);
 
